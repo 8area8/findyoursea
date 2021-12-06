bbee2e272d8f7c87d6260311dad4731a7e12ac26
mikael <mbriolet.ma@gmail.com>
2021-12-07 00:27:59 +0100

    add backend  registration
    + the registration needs the email and the password
    + username is not used

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,9 +1,23 @@
 """Note: it's a good practice to override the Abstract user."""
 
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 # from django.db import models
 
 
 class User(AbstractUser):
     """Base user."""
+
+    username = models.CharField(
+        "username",
+        max_length=150,
+        unique=False,
+        default="",
+        blank=True,
+    )
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+"""Basic auth view."""
 
-# Create your views here.
+from rest_framework import mixins
+from rest_framework import viewsets
+
+from .models import User
+from .serializers import UserSerializer
+
+
+class Signup(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    """Create a user."""
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
